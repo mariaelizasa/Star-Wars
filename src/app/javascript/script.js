02d913d5eaf1483b02b07daf4fc92121b0503772
zwingad <zwingad@gmail.com>
2019-09-19 22:33:06 -0300

    js animation, update second-content

--- src/app/javascript/script.js
+++ src/app/javascript/script.js
@@ -4,3 +4,15 @@ const $menuMobile = document.querySelector('.menu-mobile');
 $buttonMenu.addEventListener('click', function() {
   $menuMobile.classList.toggle('show');
 });
+
+function typeWrite(elemento) {
+  const textArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  textArray.forEach((letra, i) => {
+    setTimeout(function() {
+      elemento.innerHTML += letra;
+    }, 80 * i);
+  });
+}
+const title = document.querySelector('h1');
+typeWrite(title);
